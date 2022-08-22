66e41f5328c2ce12f9a73c82edaf7627484a40cf
Reqvite <reqvite2233@gmail.com>
2022-08-22 17:42:09 +0300

    add task 1,2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    galleryContainer: document.querySelector(".gallery"),
+}
+
+const galleryItemsMarkup = galleryItems.map(item => {
+    const {preview, original, description} = item
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+}).join("")
+
+refs.galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup)
+refs.galleryContainer.addEventListener("click", onClick)
+
+function onClick(e) {
+    e.preventDefault()
+    
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    } 
+
+  lightBoxTrigger(e.target.dataset.source)   
+}
+    
+function lightBoxTrigger(link) {
+  const instance = basicLightbox.create(`
+    <img src="${link}">`)
+  instance.show()
+  
+   refs.galleryContainer.addEventListener("keydown", e => {
+    
+      if (e.code === "Escape") {
+          instance.close()
+    }
+})
+}
+
+
+      
+ 
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    galleryContainer: document.querySelector(".gallery"),
+}
+
+const galleryItemsMarkup = galleryItems.map(item => {
+    const {preview, original, description} = item
+    return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`
+}).join("")
+
+
+refs.galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup)
+
+var lightbox = new SimpleLightbox('.gallery .gallery__item', {captionsData:'alt',captionDelay:250});
